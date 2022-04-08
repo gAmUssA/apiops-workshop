08a9eea8de04fc280982ac34a1d7f4d138a15d34
Viktor Gamov <gamussa@gmail.com>
2022-04-08 12:12:27 -0400

    added webfilter to get pod and namespace info

--- src/test/java/io/kong/developer/apiops/ApiopsWorkshopApplicationTests.java
+++ src/test/java/io/kong/developer/apiops/ApiopsWorkshopApplicationTests.java
@@ -1,10 +1,15 @@
 package io.kong.developer.apiops;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiopsWorkshopApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/io/kong/developer/apiops/rest/PodsWebFilterTest.java
@@ -0,0 +1,33 @@
+package io.kong.developer.apiops.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import io.kong.developer.generated.devnexus.model.Session;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PodsWebFilterTest {
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Test
+  public void filterShouldAddResponseHeaders() {
+    EntityExchangeResult<Session> result = webTestClient.get()
+        .uri("/sessions/1")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody(Session.class)
+        .returnResult();
+
+    assertThat(result.getResponseHeaders().getFirst("X-Pod-Name")).isNotEmpty();
+  }
+
+}
