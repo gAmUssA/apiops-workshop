945e4b517617cf25ca3de3ac7e0db33e27fa02ab
Viktor Gamov <gamussa@gmail.com>
2023-12-04 10:45:18 -0500

    add a quick doc

--- konnect/create-secrets.sh
+++ konnect/create-secrets.sh
@@ -1,3 +1,3 @@
-kubectl create secret tls kong-cluster-cert -n kong \
-   --cert=tls.crt \
-   --key=tls.key
+kubectl create secret tls kong-cluster-cert -n konnect-kong \
+   --cert=./tls.crt \
+   --key=./tls.key
